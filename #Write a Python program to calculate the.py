c8b05a8a24185156aaa33409b6a92e2cad89ceaf
kajalmehta1 <112338925+kajalmehta1@users.noreply.github.com>
2022-08-28 22:40:35 +0530

    Add files via upload

---
+++ #Write a Python program to calculate the.py
@@ -0,0 +1,17 @@
+#Write a Python program to calculate the length of a string
+
+#a=input("enter the string")
+
+#print(len(a))
+
+
+
+#Write a Python program to calculate the length of a string(with out len()function
+
+str=input("enter the string: ")
+
+counter=0
+for s in str:
+    counter=counter+1
+print("length of the input string: ",counter)
+
